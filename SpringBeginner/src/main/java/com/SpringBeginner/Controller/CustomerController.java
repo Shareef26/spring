55e7ee10a205fd470d2cf774abdadbdfc3649110
91966 <91966@LAPTOP-M2QT7OLH>
2020-08-07 21:13:51 +0530

    @RequestParam

--- SpringBeginner/src/main/java/com/SpringBeginner/Controller/CustomerController.java
+++ SpringBeginner/src/main/java/com/SpringBeginner/Controller/CustomerController.java
@@ -1,13 +1,16 @@
 package com.SpringBeginner.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.SpringBeginner.DTO.CustomerDTO;
@@ -23,9 +26,63 @@ public class CustomerController {
 	
 	
 @GetMapping(produces = "application/json")
-public List<CustomerDTO> fetchCustomer(){
+public List<CustomerDTO> fetchCustomers(){
 	return customerservice.fetchCustomer();
 }
+
+// Request Parameter example. cant have 2 GET methods with same req mappings
+@RequestMapping("/names")
+@GetMapping(produces= "application/json")
+public CustomerDTO fetchCustomer(@RequestParam("name") String name ) {
+	CustomerDTO custnull=null;
+	
+	System.out.println("Hi I am in names");
+	System.out.println(name);
+	
+	List<CustomerDTO> custlist=new ArrayList<CustomerDTO>();
+	custlist=customerservice.fetchCustomer();
+	System.out.println("list fetched");
+	
+	for(CustomerDTO c:custlist) {
+		System.out.println("passed name  " +name);
+		System.out.println("list name  "+ c.getName());
+		System.out.println("Comparing");
+		 if(name.equals(c.getName())) {
+			 System.out.println("Hi I am in for");
+			return c;
+		}
+	}
+	System.out.println("returning null");
+	return custnull;
+}
+
+
+/*@RequestMapping("/phone")
+@GetMapping(value="{/phone}", produces = "application/json")
+public CustomerDTO fetchCustomer1(@PathVariable long phone) {
+CustomerDTO custnull=null;
+	
+	System.out.println("Hi I am in names");
+	System.out.println(phone);
+	
+	List<CustomerDTO> custlist=new ArrayList<CustomerDTO>();
+	custlist=customerservice.fetchCustomer();
+	System.out.println("list fetched");
+	
+	for(CustomerDTO c:custlist) {
+		System.out.println("passed name  " +phone);
+		System.out.println("list name  "+ c.getName());
+		System.out.println("Comparing");
+		 if(phone==c.getPhonNo()) {
+			 System.out.println("Hi I am in for");
+			return c;
+		}
+	}
+	System.out.println("returning null");
+	return custnull;	
+}
+
+*/
 
 @PostMapping(consumes = "application/json")
 public ResponseEntity<String> createCustomer(@RequestBody CustomerDTO customerdto) {
